e67c39b97175e839bcea05bfdeb61d5c595d0861
zerohoney <zerohoney97@gmail.com>
2023-11-14 13:16:09 +0900

    prettier 정리

--- src/app/(page)/main/_contents/LatestBlocksItem.tsx
+++ src/app/(page)/main/_contents/LatestBlocksItem.tsx
@@ -1,20 +1,31 @@
-import React from 'react'
-import BlockInfo from './BlockInfo'
-import Miner from './Miner'
-import TransactionsNumber from './TransactionsNumber'
-import MainEthValue from '@app/_components/mainEthValue'
-import { IBlocksItemProps } from '../interface'
+import React from "react";
+import BlockInfo from "./BlockInfo";
+import Miner from "./Miner";
+import TransactionsNumber from "./TransactionsNumber";
+import MainEthValue from "@app/_components/mainEthValue";
+import { IBlocksItemProps } from "../interface";
 
-const LatestBlocksItem:React.FC<IBlocksItemProps> = ({blocksItem}) => {
-  const {blockHeight, blockTime, feeRecipient, transactionsInThisBlock, transactionsTime, blockReward} = blocksItem;
+const LatestBlocksItem: React.FC<IBlocksItemProps> = ({ blocksItem }) => {
+  const {
+    blockHeight,
+    blockTime,
+    feeRecipient,
+    transactionsInThisBlock,
+    transactionsTime,
+    blockReward,
+  } = blocksItem;
   return (
-    <li className='bg-white w-11/12 m-auto border-gray text-sm border-b-[1px] last:border-b-0 py-[15px]'>
+    <li className="bg-white w-11/12 m-auto border-gray text-sm border-b-[1px] last:border-b-0 py-[15px]">
       <BlockInfo blockHeight={blockHeight} blockTime={blockTime} />
-      <Miner feeRecipient = {feeRecipient} />
-      <TransactionsNumber blockHeight={blockHeight} transactionsInThisBlock={transactionsInThisBlock} transactionsTime={transactionsTime} />
+      <Miner feeRecipient={feeRecipient} />
+      <TransactionsNumber
+        blockHeight={blockHeight}
+        transactionsInThisBlock={transactionsInThisBlock}
+        transactionsTime={transactionsTime}
+      />
       <MainEthValue ethValue={blockReward} />
     </li>
-  )
-}
+  );
+};
 
-export default LatestBlocksItem
+export default LatestBlocksItem;
