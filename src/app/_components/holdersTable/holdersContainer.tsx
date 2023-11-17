c72498fdc340853ef8ded65892617091f0f89600
biggsky <sunghyun0624@naver.com>
2023-11-17 19:01:06 +0900

    test

---
+++ src/app/_components/holdersTable/holdersContainer.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { IholdersList, HoldersListProps } from "./interface";
+import HoldersItem from "./HoldersItem";
+import Pagination from "@app/_components/pagination/Pagiation";
+import usePagiNation from "@app/_hooks/usePagination";
+
+const HoldersContainer: React.FC<HoldersListProps> = ({ holdersList }) => {
+  const { maxPage, page, pageHandler, pageTxList } = usePagiNation(holdersList);
+  return (
+    <div>
+      <div>
+        {pageTxList &&
+        pageTxList .map((ele) => (
+            <HoldersItem HoldersItem={ele} key={ele.rank} />
+          ))}
+      </div>
+     
+    </div>
+  );
+};
+
+export default HoldersContainer;
