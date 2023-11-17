e828441bbf3fda4d29c8354009bce5b1fb573d61
zerohoney <zerohoney97@gmail.com>
2023-11-17 18:09:53 +0900

    깃허브 대,소문자 구별

---
+++ src/app/(page)/nft/_contents/NftItem.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NftItem = () => {
+  return (
+    <div className="w-full flex justify-between">
+      <div className="text-lg font-bold">1</div>
+      <div className="text-lg font-bold">super nft</div>
+    </div>
+  );
+};
+
+export default NftItem;
