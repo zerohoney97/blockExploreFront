490e6b328e5be08c105eae80a9fce15adf62fa1a
biggsky <sunghyun0624@naver.com>
2023-11-17 18:38:32 +0900

    1117 push

--- src/app/(page)/nft/detail/[nftItem]/_contents/activity/ItemActivity.tsx
+++ src/app/(page)/nft/detail/[nftItem]/_contents/activity/ItemActivity.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 const NftItemActivity = () => {
   return (
     <div className='bg-yellow h-[100px] rounded-xl'>
-      
+      {/* <ActivityTitle /> */}
+      {/* <ActivityList /> */}
     </div>
   )
 }
