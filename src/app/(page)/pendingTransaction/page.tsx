3485b07ce658e7d634786b0fd8fde849436b165d
zerohoney <zerohoney97@gmail.com>
2023-11-23 13:57:34 +0900

    페이지 수정

--- src/app/(page)/pendingTransaction/page.tsx
+++ src/app/(page)/pendingTransaction/page.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import PendingTxListWrap from "./_contents";
 import useHydration from "@app/_hooks/useHydration";
-import { getTransaction } from "@app/_api/main/getTransaction";
-import { getBlock } from "@app/_api/main/getBlock";
 
 const Page = async () => {
-  const td = await getTransaction();
   return (
     <div>
       <PendingTxListWrap />
