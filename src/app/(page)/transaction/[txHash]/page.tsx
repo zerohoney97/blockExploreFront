77db3aed6de7b473ee5c93130c2c9201909df900
zerohoney <zerohoney97@gmail.com>
2023-11-27 20:01:38 +0900

    asd

--- src/app/(page)/transaction/[txHash]/page.tsx
+++ src/app/(page)/transaction/[txHash]/page.tsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import ItemTitle from "@app/_components/itemTitle";
 import TxTabButtonWrap from "./_contents/TabButtonWrap";
 import { getTxItemData } from "@app/_api/txDetail/getTxItemData";
-const Page = ({ params }: { params: { txHash: string } }) => {
-  const transactionItemData = getTxItemData(params.txHash);
-  
+import { getTransaction } from "@app/_api/main/getTransaction";
+const Page = async ({ params }: { params: { txHash: string } }) => {
+  const transactionItemData = await getTxItemData('2');
+
+  // const transactionData = await getTransaction('main');
   return (
     <div className="bg-mainBackGroundColor pb-5  max-w-[1250px] m-auto dark:bg-black/90">
       <ItemTitle title="Transaction Details" />
--- src/app/_api/txDetail/getTxItemData.tsx
+++ src/app/_api/txDetail/getTxItemData.tsx
@@ -1,8 +1,18 @@
 export const getTxItemData = async (txHash: string) => {
-  console.log(txHash);
-  const responseTransactionData = await fetch(`${  process.env.NODE_ENV === "development"
-  ? "http://localhost:8080"
-  : "https://api.bouncexplorer.site"}/tx/2`);
-  const transactionData = await responseTransactionData.json();
-  console.log(transactionData);
+  try {
+    console.log(txHash);
+    const responseTransactionData = await fetch(
+      `${
+        process.env.NODE_ENV === "development"
+          ? "http://localhost:8080"
+          : "https://api.bouncexplorer.site"
+      }/tx/${txHash}`
+    );
+
+    // console.log(responseTransactionData.json());
+    const transactionData: any = await responseTransactionData.json();
+    return "transactionData";
+  } catch (error) {
+    console.log(error);
+  }
 };
