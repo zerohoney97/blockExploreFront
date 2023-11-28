a8909dea9fd78f5c3724dc75a31264d6cd37cbd2
yeonsu <gys2116@gmail.com>
2023-11-28 15:51:49 +0900

    errorpage

---
+++ src/app/(page)/error/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const page = () => {
+  return (      
+        <p className="text-red-500 text-lg font-semibold py-10">요청하신 내용을 확인할 수 없습니다</p>
+
+  );
+}
+
+export default page
