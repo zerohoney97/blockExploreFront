a93ec87c3312747f1c52c9b491fa9025ef96f678
zerohoney <zerohoney97@gmail.com>
2023-12-04 14:38:23 +0900

    extra data

--- src/app/(page)/blocks/[blockdetail]/_contents/CardText.tsx
+++ src/app/(page)/blocks/[blockdetail]/_contents/CardText.tsx
@@ -19,10 +19,10 @@ const CardText: React.FC<CardTextProps> = ({
     <div
       className={`my-3 break-words ${type && "sm:h-[80px]"}  md:flex lg:flex`}
     >
-      <div className="font-medium text-sm md:text-itemDetail-textLabelColor mr-2 w-36 lg:text-itemDetail-textLabelColor dark:text-itemDetail-textLabelColor">
+      <div className="font-medium text-sm truncate md:text-itemDetail-textLabelColor mr-2 w-36 lg:text-itemDetail-textLabelColor dark:text-itemDetail-textLabelColor">
         {title}
       </div>
-      <span className="text-xs truncate dark:text-white">
+      <span className="text-xs  overflow-hidden dark:text-white">
         {content}
         {content2}
         {children && <span className="text-xs">{children}</span>}
